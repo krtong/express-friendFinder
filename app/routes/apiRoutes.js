4080aed8379bb77a4d0f31d8289d43b4dd41f5b3
krtong <kevin.r.tong@gmail.com>
2020-03-27 23:38:19 -0700

    changing some stuff

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -7,23 +7,25 @@ module.exports = function(app) {
       return res.json(userDB);
   });
 
-  const calculateFriends = function (req, res) {
-
-    const newUser = req.body;
+  const calculateFriends = function ({body: newUser}, res) {
     const {scores} = newUser;
-    let minDiff = Infinity;
+    
     const scoreArr = userDB.map((user, i) => {
       const compareArr = user.scores.map((a, i) => Math.abs(a - scores[i]));
-      console.log({compareArr})
       const score = compareArr.reduce((sum, num) => num + sum);
       return score;
     });
-    
+
     const idx = scoreArr.reduce((j, cur, i, arr) => arr[j] >= cur ? i : j, 0);
+
     const bestUser = userDB[idx];
-    const bestScore = scoreArr[idx]
-    const userScore = scores.reduce((acc, cur) =>  parseInt(cur) + acc, 0)
+
+    const bestScore = scoreArr[idx];
+
+    const userScore = scores.reduce((sum, num) =>  parseInt(num) + sum, 0);
+
     const matchPercentage = ((userScore- bestScore)/userScore*100).toFixed(0);
+
     userDB.push(newUser);
     res.json({bestUser, matchPercentage})
   }
